fb64d094a3f091b789e5c146e770a88f4760c1b9
Paul Vick <paulv@microsoft.com>
2020-09-21 21:41:00 -0700

    Update to newer version

--- src/Tutorial/02_Event_Driven_Programming/Program.cs
+++ src/Tutorial/02_Event_Driven_Programming/Program.cs
@@ -7,20 +7,17 @@ Size windowSize = (640, 480);
 Rectangle windowRectangle = (Window.UndefinedWindowLocation, windowSize);
 using var window = Window.Create("Event Driven Programming", windowRectangle, WindowFlags.Shown);
 
-Keyboard.KeyDown += (s, e) =>
+Keyboard.KeyDown += (s, e) => Application.ShowMessageBox(MessageBoxFlags.Information, "Key Press", e.Keycode switch
 {
-    app.ShowMessageBox(MessageBoxFlags.Information, "Key Press", e.Keycode switch
-    {
-        Keycode.Up => "Up!",
-        Keycode.Down => "Down!",
-        Keycode.Left => "Left!",
-        Keycode.Right => "Right!",
-        _ => "Other!"
-    }, window);
-};
+    Keycode.Up => "Up!",
+    Keycode.Down => "Down!",
+    Keycode.Left => "Left!",
+    Keycode.Right => "Right!",
+    _ => "Other!"
+}, window);
 
-Mouse.ButtonDown += (s, e) => app.ShowMessageBox(MessageBoxFlags.Information, "Mouse Button", "Down!", window);
-Mouse.ButtonUp += (s, e) => app.ShowMessageBox(MessageBoxFlags.Information, "Mouse Button", "Up!", window);
+Mouse.ButtonDown += (s, e) => Application.ShowMessageBox(MessageBoxFlags.Information, "Mouse Button", "Down!", window);
+Mouse.ButtonUp += (s, e) => Application.ShowMessageBox(MessageBoxFlags.Information, "Mouse Button", "Up!", window);
 
 while (app.DispatchEvent())
 {
