b85463e3d1bf42304685aba0516f309ab6e14f86
Paul Vick <paul@panopticoncentral.net>
2022-12-11 23:46:13 -0800

    Managed clipboard sample

---
+++ src/Samples/Managed/SDL_clipboard/Clipboard/Program.cs
@@ -0,0 +1,35 @@
+﻿using SdlSharp;
+
+namespace Samples
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using var applications = new Application(Subsystems.Video);
+
+            var exit = false;
+            while (!exit)
+            {
+                var command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "q":
+                        exit = true;
+                        break;
+
+                    case "c":
+                        Clipboard.Text = Console.ReadLine();
+                        Console.WriteLine("Set clipboard text.");
+                        break;
+
+                    case "p":
+                        Console.WriteLine(Clipboard.Text);
+                        Console.WriteLine("Read clipboard text.");
+                        break;
+                }
+            }
+        }
+    }
+}
